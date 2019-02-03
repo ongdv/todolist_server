5af57ca0edcc998fcc5f7b44d1e3308ed97afd12
ongdv <ssokwoo@outlook.com>
2019-02-03 21:49:39 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,119 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+/* GET home page. */
+router.get('/list', function(req, res, next) {
+	var sql = "SELECT * FROM todo";
+	conn.query(sql, (err, row) => {
+		if(err){
+			throw err;
+		}
+		res.send(200, {
+			result: 1,
+			data: row
+		});
+	})
+});
+
+router.get('/mylist', function(req, res, next) {
+	var sql = "SELECT * FROM todo WHERE mail = ?";
+	conn.query(sql, [req.query.mail], (err, row) => {
+		if(err){
+			throw err;
+		}
+		res.send(200, {
+			result: 1,
+			data: row
+		});
+	})
+});
+
+router.post('/addTodo', (req, res, next) => {
+	var postData = req.body;
+	var sql = "INSERT INTO todo (mail, todo, done) VALUES (?,?,?)";
+	conn.query(sql, [postData.mail, postData.todo, postData.done], (err, result) => {
+		if(err){
+			throw err;
+		}
+		if(!result){
+			res.send(300, {
+				result: 0,
+				msg: "addTodo Failure"
+			})
+		}else{
+			res.send(200, {
+				result: 1,
+				msg: "addTodo Success"
+			})
+		}
+	})
+})
+
+router.get('/deleteTodo/:id', (req, res, next) => {
+	var sql = "DELETE FROM todo WHERE idx = ?"
+	conn.query(sql, [req.params.id], (err, result) => {
+		if(err){
+			throw err;
+		}
+		if(!result) {
+			res.send(300, {
+				result: 0,
+				msg: "DeleteTodo Failure"
+			});
+		}else{
+			res.send(200, {
+				result:1,
+				msg: "DeleteTodo Success"
+			})
+		}
+	})
+})
+
+router.get('/doneTodo/:id', (req, res, next) => {
+	var done;
+	var sql = "SELECT * FROM todo WHERE idx = ?";
+	conn.query(sql, [req.params.id], (err, row) => {
+		if(err){
+			throw err;
+		}
+		if(row[0].done === 0){
+			done = 1;
+		}else{
+			done = 0;
+		}
+		
+		var sql = "UPDATE todo SET done=? WHERE idx = ?";
+		conn.query(sql, [done, req.params.id], (err, result) => {
+			if(err){
+				throw err;
+			}
+			if(!result) {
+				res.send(300, {
+					result: 0,
+					msg: "changeDone Failure"
+				});
+			}else{
+				res.send(200, {
+					result:1,
+					msg: "changeDone Success"
+				})
+			}
+		})
+	});
+	
+})
+
+// router.post('/editTodo/:id', (req, res, next) => {
+// 	var postData = req.body;
+// 	var sql = "SELECT * FROM todo WHERE mail = ? AND idx = ?";
+// 	conn.query(sql, [postData.mail, req.params.id], (err, row) => {
+// 		if(err){
+// 			throw err;
+// 		}
+		
+// 	})
+// })
+
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var router = express.Router();
+var pool = require('../config/dbConfig');
+
+/* GET users listing. */
+router.get('/list', function(req, res, next) {
+	var sql = "SELECT * FROM user";
+	conn.query(sql, (err, row) => {
+		if(err){
+			res.send(300, {
+				result: 0,
+				msg: err
+			})
+		}
+		res.send(200, {
+			result: 1,
+			data: row
+		})
+	})
+});
+
+router.post('/signup', (req, res, next) => {
+	var postData = req.body;
+	if(postData.mail === "" || postData.password === "" || postData.username ===""){
+		res.send(300, {
+			result: 0,
+			msg: "check the blank"
+		});
+	}
+	var sql ="SELECT * FROM user WHERE mail = ?";
+	conn.query(sql, [postData.mail], (err, row) => {
+		if(err){
+			res.send(300, {
+				result: 0,
+				msg: err
+			});
+		}
+		if(row.length === 0){
+			var sql = "INSERT INTO user VALUES (?, ?, ?)";
+			conn.query(sql, [postData.mail, postData.password, postData.username], (err, result) => {
+				if(err){
+					throw err;
+				}
+
+				if(result){
+					res.send(200, {
+						result: 1,
+						msg : "SignUp Success"
+					});
+				}else{
+					res.send(300, {
+						result: 0,
+						msg: "SignUp Failure"
+					});
+				}
+			});
+		}else{
+			res.send(300, {
+				result: 0,
+				msg: "Invalid Data"
+			});
+		}
+	});
+});
+
+
+router.post('/signin', (req, res, next) => {
+	var postData = req.body;
+	if(postData.mail === "" || postData.password === ""){
+		res.send(300, {
+			result: 0,
+			msg: "check the blank"
+		});
+	}
+	var sql = "SELECT * FROM user WHERE mail = ? AND password = ?";
+	conn.query(sql, [postData.mail, postData.password], (err, row) => {
+		if(err){
+			throw err;
+		}
+
+		if(row.length === 0){
+			res.send(300, {
+				result: 0,
+				msg: "Invalid Data"
+			});
+		}else{
+			res.send(200, {
+				result: 1,
+				data: row
+			});
+		}
+	});
+});
+
+
+module.exports = router;
